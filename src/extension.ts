52360b1ae89b435ddee9da0d6ee497740be3b6e6
Mark Lopez <m@silvenga.com>
2017-04-24 18:03:50 -0500

    Shamelessly bootstrapped base from Huachao/vscode-restclient.

---
+++ src/extension.ts
@@ -0,0 +1,18 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { ExtensionContext, commands, languages, TextDocument, Range } from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export async function activate(context: ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Active!');
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
