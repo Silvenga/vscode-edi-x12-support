c291663ab3e9b6d68a0476bae243b16c354a9441
Mark Lopez <m@silvenga.com>
2017-04-28 22:52:22 -0500

    Added prettify/uglify commands.

--- src/controllers/commandsController.ts
+++ src/controllers/commandsController.ts
@@ -6,7 +6,8 @@ export class CommandsController implements Disposable {
 
     public bind(context: ExtensionContext) {
         context.subscriptions.push(this);
-        context.subscriptions.push(commands.registerCommand('edi-x12-support.add-new-lines', () => this.addNewLines()));
+        context.subscriptions.push(commands.registerCommand('edi-x12-support.prettify', () => this.prettify()));
+        context.subscriptions.push(commands.registerCommand('edi-x12-support.uglify', () => this.uglify()));
     }
 
     private setLanguage() {
@@ -14,13 +15,38 @@ export class CommandsController implements Disposable {
         // window.activeTextEditor.document.languageId = ""
     }
 
-    public addNewLines() {
+    public prettify() {
 
         let parser = new Parser();
         let document = window.activeTextEditor.document.getText();
         var config = parser.parseHeader(document);
+        if (config == null) {
+            window.showErrorMessage("No ISA header found.");
+            return;
+        }
+
         let segments = parser.parseSegments(document, config);
         let text = segments.join("\n");
+
+        window.activeTextEditor.edit(builder => {
+            let start = window.activeTextEditor.document.positionAt(segments[0].startIndex);
+            let end = window.activeTextEditor.document.positionAt(segments[segments.length - 1].endIndex);
+            builder.replace(new Range(start, end), text);
+        })
+    }
+
+    public uglify() {
+
+        let parser = new Parser();
+        let document = window.activeTextEditor.document.getText();
+
+        var config = parser.parseHeader(document);
+        if (config == null) {
+            window.showErrorMessage("No ISA header found.");
+            return;
+        }
+        let segments = parser.parseSegments(document, config);
+        let text = segments.join("");
 
         window.activeTextEditor.edit(builder => {
             let start = window.activeTextEditor.document.positionAt(segments[0].startIndex);
