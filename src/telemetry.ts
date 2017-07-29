1567be2da0903a9174d9ff78f33d8ca877e12441
Mark Lopez <m@silvenga.com>
2017-07-29 14:06:09 -0500

    Added ability to capture events.

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'inversify';
+import PiwikTracker = require('piwik-tracker');
 import * as Raven from 'raven';
-import { Error } from 'tslint/lib/error';
 
 import { IConfiguration } from './interfaces/configuration';
 
@@ -9,6 +9,7 @@ export class Telemetry {
 
     private _ravenStatic: Raven.RavenStatic;
     private _configuration: IConfiguration;
+    private _piwikTracker: PiwikTracker;
 
     public constructor( @inject('IConfiguration') configuration: IConfiguration) {
         this._configuration = configuration;
@@ -16,10 +17,28 @@ export class Telemetry {
 
     public install() {
         this._ravenStatic = Raven.config(this._configuration.ravenDsn);
+        this._piwikTracker = new PiwikTracker(this._configuration.piwikSiteId, this._configuration.piwikUrl);
     }
 
     // tslint:disable-next-line:no-any
     public captureException(error: any) {
         this._ravenStatic.captureException(error);
+    }
+
+    public captureEvent(action: string) {
+        return new Promise((resolve, reject) => {
+            try {
+                this._piwikTracker.track({
+                    url: `https://vscode.silvenga.com/edi-support?action=${encodeURIComponent(action)}`,
+                    action_name: action,
+                    ua: `VSCode v${this._configuration.vsCodeVersion}`,
+                    uid: this._configuration.vsCodeMachineId,
+                    lang: this._configuration.vscodeLanguage
+                });
+                console.log(`Captured event ${action}.`);
+            } catch (error) {
+                this.captureException(error);
+            }
+        });
     }
 }
