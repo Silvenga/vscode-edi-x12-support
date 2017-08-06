32be41be3cd2808914f87c4ea6ca0199ff9db3ed
Mark Lopez <m@silvenga.com>
2017-08-06 00:08:43 -0500

    Updated configuration.ts to be easier to mock via testdouble.

--- src/interfaces/configuration.ts
+++ src/interfaces/configuration.ts
@@ -1,17 +1,17 @@
 
 export interface IConfiguration {
-    readonly languageId: string;
-    readonly dataElementSeparator: string;
-    readonly componentElementSeparator: string;
-    readonly repetitionElementSeparator: string;
-    readonly segmentSeparator: string;
-    readonly ravenDsn: string;
-    readonly piwikSiteId: number;
-    readonly piwikUrl: string;
-    readonly devMode: boolean;
-    readonly vsCodeMachineId: string;
-    readonly vscodeLanguage: string;
-    readonly vsCodeVersion: string;
-    readonly telemetryDisabled: boolean;
-    readonly extensionVersion: string;
+    languageId: string;
+    dataElementSeparator: string;
+    componentElementSeparator: string;
+    repetitionElementSeparator: string;
+    segmentSeparator: string;
+    ravenDsn: string;
+    piwikSiteId: number;
+    piwikUrl: string;
+    devMode: boolean;
+    vsCodeMachineId: string;
+    vscodeLanguage: string;
+    vsCodeVersion: string;
+    telemetryDisabled: boolean;
+    extensionVersion: string;
 }
